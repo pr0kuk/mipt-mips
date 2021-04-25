dc42db0a054c06e99d4d157f5b8f57defb8ba7e5
pr0kuk <shcherbakov.aa@phystech.edu>
2021-04-25 21:54:15 +0300

    tests passed

--- simulator/func_sim/ooo_window/ooo_window.h
+++ simulator/func_sim/ooo_window/ooo_window.h
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <list>
-#include <func_sim/operation.h>
 template <typename FuncInstr> 
 class OOOWindow {
     private:
         unsigned long size;
-        int number_of_independent_instructions;
-        int number_of_updates;
+        double number_of_independent_instructions;
+        double number_of_updates;
         std::list <FuncInstr> OOOQueue;
     public:
         OOOWindow(unsigned long number) {
@@ -15,30 +14,45 @@ class OOOWindow {
             number_of_updates = 0;
         }
         void write_instruction(FuncInstr instruction) {
+            if (size == 0)
+                return;
             if (static_cast<unsigned long>(OOOQueue.size()) == size) {
                 OOOQueue.pop_back();
                 OOOQueue.push_front(instruction);
-                int count = 0;
-                for (auto it1 = OOOQueue.begin(), it2 = OOOQueue.end(); it1 != it2; ++it1) {
-                    for (auto it3 = OOOQueue.begin(); it3 != it1; ++it3) {
+                //int flag = 0;
+                std::list <FuncInstr> templist = OOOQueue;
+                for (auto it1 = templist.begin(); it1 != templist.end(); ++it1) {
+                    for (auto it3 = it1; it3 != templist.end(); ++it3) {
+                        if (it3 == it1)
+                            continue;
+                        //flag = 0;
                         int temp = 0;
-                        for (int k = 0; k < 3; k++)
-                            if (!((*it3).get_src_num(k) == (*it1).get_dst_num()) || ((*it3).get_src_num(k).is_zero() && (*it1).get_dst_num().is_zero()))
+                        for (int k = 0; k < 3; k++) {
+                            if (((*it3).get_src_num(k) == (*it1).get_dst_num()) && !((*it3).get_src_num(k).is_zero()) && !((*it1).get_dst_num().is_zero())) {
                                 temp++;
-                        if (temp == 3)
-                            count += 2;
+                                it3 = templist.erase(it3);
+                                it3--;
+                                //flag = 1;
+                            }   
+                        }
                     }
+                    /*if (flag) {
+                        it1 = templist.erase(it1);
+                        it1--;
+                    }*/
                 }
-                number_of_independent_instructions += count;
+                number_of_independent_instructions += templist.size();
                 number_of_updates++;
                 return;
             }
             OOOQueue.push_front(instruction);
         }
         double get_avg_independent_instructions() {
+            if (number_of_updates == 0)
+                return 0;
             return static_cast<double>(number_of_independent_instructions/number_of_updates);
         }
-        ~OOOWindow() {
-            std::cout << static_cast<double>(number_of_independent_instructions/number_of_updates) << std::endl;
-        }
+        //~OOOWindow() {
+        //    std::cout << this->get_avg_independent_instructions() << std::endl;
+        //}
 };
