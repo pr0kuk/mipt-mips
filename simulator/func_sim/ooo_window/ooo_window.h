21fa1ce960e01dee5f7a6fb2a226ec1187643ec6
pr0kuk <shcherbakov.aa@phystech.edu>
2021-04-25 20:28:06 +0300

    Fixed get_avg_independent_instructions() for tests

--- simulator/func_sim/ooo_window/ooo_window.h
+++ simulator/func_sim/ooo_window/ooo_window.h
@@ -18,28 +18,27 @@ class OOOWindow {
             if (static_cast<unsigned long>(OOOQueue.size()) == size) {
                 OOOQueue.pop_back();
                 OOOQueue.push_front(instruction);
-                this->get_avg_independent_instruction();
+                int count = 0;
+                for (auto it1 = OOOQueue.begin(), it2 = OOOQueue.end(); it1 != it2; ++it1) {
+                    for (auto it3 = OOOQueue.begin(); it3 != it1; ++it3) {
+                        int temp = 0;
+                        for (int k = 0; k < 3; k++)
+                            if (!((*it3).get_src_num(k) == (*it1).get_dst_num()) || ((*it3).get_src_num(k).is_zero() && (*it1).get_dst_num().is_zero()))
+                                temp++;
+                        if (temp == 3)
+                            count += 2;
+                    }
+                }
+                number_of_independent_instructions += count;
+                number_of_updates++;
                 return;
             }
             OOOQueue.push_front(instruction);
         }
-        double get_avg_independent_instruction() {
-            int count = 0;
-            for (auto it1 = OOOQueue.begin(), it2 = OOOQueue.end(); it1 != it2; ++it1) {
-                for (auto it3 = OOOQueue.begin(); it3 != it1; ++it3) {
-                    int temp = 0;
-                    for (int k = 0; k < 3; k++)
-                        if (!((*it3).get_src_num(k) == (*it1).get_dst_num()) || ((*it3).get_src_num(k).is_zero() && (*it1).get_dst_num().is_zero()))
-                            temp++;
-                    if (temp == 3)
-                        count += 2;
-                }
-            }
-            number_of_independent_instructions += count;
-            number_of_updates++;
+        double get_avg_independent_instructions() {
             return static_cast<double>(number_of_independent_instructions/number_of_updates);
         }
         ~OOOWindow() {
-                std::cout << static_cast<double>(number_of_independent_instructions/number_of_updates) << std::endl;
+            std::cout << static_cast<double>(number_of_independent_instructions/number_of_updates) << std::endl;
         }
 };
