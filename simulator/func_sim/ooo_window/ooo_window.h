87e81387d1e96efaeebddca5eda7d22ffc86b63b
pr0kuk <shcherbakov.aa@phystech.edu>
2021-04-26 18:28:37 +0300

    Remove templist, improve code readability

--- simulator/func_sim/ooo_window/ooo_window.h
+++ simulator/func_sim/ooo_window/ooo_window.h
@@ -8,42 +8,39 @@ class OOOWindow {
         unsigned long long number_of_updates;
         std::list <FuncInstr> OOOQueue;
     public:
-        OOOWindow(unsigned long number) {
-            size = number;
-            number_of_independent_instructions = 0;
-            number_of_updates = 0;
-        }
+        OOOWindow(unsigned long number): size(number), number_of_independent_instructions(0), number_of_updates(0) {}
+
         void write_instruction(FuncInstr instruction) {
             if (size == 0)
                 return;
             if (static_cast<unsigned long>(OOOQueue.size()) == size) {
-                OOOQueue.pop_back();
-                OOOQueue.push_front(instruction);
-                std::list <FuncInstr> templist = OOOQueue;
-                for (auto it1 = templist.begin(); it1 != templist.end(); ++it1) {
-                    for (auto it3 = it1; it3 != templist.end(); ++it3) {
-                        if (it3 == it1)
-                            continue;
-                        for (int k = 0; k < 3; k++) {
-                            if (((*it3).get_src_num(k) == (*it1).get_dst_num()) && !((*it3).get_src_num(k).is_zero()) && !((*it1).get_dst_num().is_zero())) {
-                                it3 = templist.erase(it3);
-                                it3--;
-                            }   
-                        }
-                    }
-                }
-                number_of_independent_instructions += templist.size();
-                number_of_updates++;
+                OOOQueue.pop_front();
+                OOOQueue.push_back(instruction);
+                this->update_number_of_independent_instructions();
                 return;
             }
-            OOOQueue.push_front(instruction);
+            OOOQueue.push_back(instruction);
         }
+
+        void update_number_of_independent_instructions() {
+            int count = 0, function_depend = 0;
+            for (auto it1 = OOOQueue.begin(), end = OOOQueue.end(); it1 != end; ++it1, function_depend = 0) {
+                for (auto it3 = OOOQueue.begin(); it3 != it1 && !function_depend; ++it3)
+                    for (int k = 0; k < 3 && !function_depend; k++)
+                        if (((*it1).get_src_num(k) == (*it3).get_dst_num()) && !((*it3).get_dst_num().is_zero()))
+                            function_depend = 1;
+                if (!function_depend)
+                    count++;
+            }
+            number_of_independent_instructions += count;
+            number_of_updates++;
+        }
+
         double get_avg_independent_instructions() {
             if (number_of_updates == 0)
                 return 0;
             return (static_cast<double>(number_of_independent_instructions))/(static_cast<double>(number_of_updates));
         }
-        ~OOOWindow() {
-            std::cout << this->get_avg_independent_instructions() << std::endl;
-        }
+
+        ~OOOWindow() { std::cout << this->get_avg_independent_instructions() << std::endl; }
 };
